11d32cff726bf99607f545a2a119328afc9fe158
Marc Lavergne <24737495+ScienceMarc@users.noreply.github.com>
2023-03-07 10:10:30 -0600

    Made AA asynchronous. Made smaller attempts to improve performance.

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"image/color"
-	"log"
 	"math"
-	"os"
-	"runtime/pprof"
+	"runtime"
 	"sync"
 	"time"
 
@@ -19,23 +17,27 @@ const (
 )
 
 func main() {
-	f, err := os.Create("cpu.pprof")
-	if err != nil {
-		log.Fatal("could not create CPU profile: ", err)
-	}
-	defer f.Close() // error handling omitted for example
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal("could not start CPU profile: ", err)
-	}
-	defer pprof.StopCPUProfile()
+	runtime.LockOSThread()
+	/*
+		f, err := os.Create("cpu.pprof")
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	*/
+
 	rl.InitWindow(int32(width), int32(height), "Julia Explorer")
 	rl.SetTargetFPS(60)
 
 	screenBuffer := rl.LoadTextureFromImage(rl.GenImageColor(rl.GetScreenWidth(), rl.GetScreenHeight(), rl.Yellow))
 	renderer := NewRenderer(width, height)
 
-	batches := []int{1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 30, 36, 40, 45, 48, 60, 72, 80, 90, 120, 144, 180, 240, 360, 720}
-	batchSize := 29
+	batches := []int{1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 30, 32, 36, 40, 45, 48, 60, 72, 80, 90, 96, 120, 144, 160, 180, 240, 288, 360, 480, 720, 1440}
+	batchIdx := 0
 	maxIters := 64
 	samples := 4.0
 	zoom := 1.0
@@ -44,57 +46,104 @@ func main() {
 
 	frameTimes := make([]float64, 180)
 
+	update := true
+	oldMouse := complex(0, 0)
+	updateFrame := 0
+	frameCount := 0
+
+	rendering := false
+	needToAA := false
+
 	for !rl.WindowShouldClose() {
 		mouseX := float64(rl.GetMouseX())
 		mouseY := float64(rl.GetMouseY())
 		mouse := complex(mapRange(mouseX, 0, width, -4, 4), mapRange(mouseY, 0, height, 2, -2))
 
-		switch rl.GetKeyPressed() {
-		case rl.KeyLeftAlt:
+		if mouse != oldMouse && !mandelbrotMode {
+			update = true
+		}
+		oldMouse = mouse
+
+		key := rl.GetKeyPressed()
+
+		switch key {
+		case 'X':
 			samples *= 4
-		case rl.KeyLeftControl:
+			needToAA = true
+		case 'Z':
 			samples = math.Max(samples/4, 1)
+			needToAA = true
 		case '=':
 			maxIters *= 2
+			update = true
 		case '-':
 			maxIters = int(math.Max(float64(maxIters/2), 1))
+			update = true
 		case 'M':
 			mandelbrotMode = !mandelbrotMode
+			update = true
 		case 'R':
-			batchSize = 29
+			//batchIdx = 0
 			maxIters = 64
-			if mandelbrotMode {
-				samples = 1.0
-			} else {
-				samples = 4.0
-			}
 			zoom = 1.0
 			offset = complex(0, 0)
+			update = true
+		case 'O':
+			batchIdx = (batchIdx + 1) % len(batches)
+			fmt.Println(batchIdx, batches[batchIdx])
+		case 'P':
+			batchIdx--
+			if batchIdx < 0 {
+				batchIdx = len(batches) - 1
+			}
+			fmt.Println(batchIdx, batches[batchIdx])
+		case 0:
+
+		default:
+			update = true
 		}
 
 		if rl.IsKeyDown(rl.KeySpace) {
 			zoom *= 1.1
+			update = true
 		}
 		if rl.IsKeyDown(rl.KeyLeftShift) {
 			zoom /= 1.1
+			update = true
 		}
 		if rl.IsKeyDown(rl.KeyD) {
 			offset += complex(0.1/zoom, 0)
+			update = true
 		}
 		if rl.IsKeyDown(rl.KeyA) {
 			offset -= complex(0.1/zoom, 0)
+			update = true
 		}
 		if rl.IsKeyDown(rl.KeyW) {
 			offset += complex(0, 0.1/zoom)
+			update = true
 		}
 		if rl.IsKeyDown(rl.KeyS) {
 			offset -= complex(0, 0.1/zoom)
+			update = true
 		}
 
 		frameTimes = append(frameTimes[1:], 1000*float64(rl.GetFrameTime()))
 
 		rl.BeginDrawing()
-		render(&renderer, mouse, batchSize, maxIters, batches, samples, zoom, offset, mandelbrotMode)
+		if update {
+			render(&renderer, mouse, batches[batchIdx], maxIters, 1.0, zoom, offset, mandelbrotMode)
+			updateFrame = frameCount
+			needToAA = true
+		} else if needToAA && !rendering && updateFrame+60 <= frameCount {
+			rendering = true
+			needToAA = false
+			go func() {
+				render(&renderer, mouse, batches[batchIdx], maxIters, samples, zoom, offset, mandelbrotMode)
+				rendering = false
+			}()
+		}
+		renderer.FillRect(width-180, 40, 180, 100, rl.DarkGray)
 		renderer.PlotRect(width-180, 40, 180, 100, rl.White)
 		for i, ft := range frameTimes {
 			x := int(mapRange(float64(i), 0, float64(len(frameTimes)), width-180, width))
@@ -111,28 +160,32 @@ func main() {
 		ft := fmt.Sprint(time.Duration(time.Millisecond * time.Duration(frameTimes[len(frameTimes)-1])))
 		rl.DrawText(ft, int32(rl.GetScreenWidth()-int(rl.MeasureText(ft, 20))), 20, 20, rl.White)
 
-		rl.DrawText(fmt.Sprintf("C=%v \ncenter: %v, zoom: %fx\n%d iterations (%.0fxSSAA)", mouse, offset, zoom, maxIters, samples), 0, height-80, 20, rl.LightGray)
+		rText := ""
+		if rendering {
+			rText = "!RENDERING!"
+		}
+		rl.DrawText(fmt.Sprintf("C=%v \ncenter: %v, zoom: %fx\n%d iterations (%.0fxSSAA) [%d threads] %s", mouse, offset, zoom, maxIters, samples, width/batches[batchIdx], rText), 0, height-80, 20, rl.LightGray)
 
 		rl.EndDrawing()
+		update = false
+		frameCount++
 	}
 }
 
-func render(r *Renderer, mouse complex128, batchSize, maxIters int, batches []int, samples, zoom float64, offset complex128, mandelbrotMode bool) {
+func render(r *Renderer, mouse complex128, batchSize, maxIters int, samples, zoom float64, offset complex128, mandelbrotMode bool) {
 	var wg sync.WaitGroup
 
-	wg.Add(width * height / batches[batchSize])
-
+	wg.Add(width / batchSize)
 	//fmt.Println(mouse, struct{ X, Y float64 }{float64(rl.GetMouseX()), float64(rl.GetMouseY())})
 	sqrt := math.Sqrt(samples)
 
 	var scale = 2.0 / zoom
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y += batches[batchSize] {
-			//TODO: Don't spawn 1440 threads
-			go func(x, y int) {
-				defer wg.Done()
-				for i := 0; i < batches[batchSize]; i++ {
-					y := y + i
+	for x := 0; x < width; x += batchSize {
+		go func(x int) {
+			defer wg.Done()
+			for i := 0; i < batchSize; i++ {
+				x := x + i
+				for y := 0; y < height; y++ {
 					iterAvg := 0.0
 					for s := 0.0; s < samples; s++ {
 						xs := float64(int(s) % int(sqrt))
@@ -154,8 +207,9 @@ func render(r *Renderer, mouse complex128, batchSize, maxIters int, batches []in
 					iterAvg /= float64(samples)
 					r.Plot(x, y, getColor(int(iterAvg), maxIters))
 				}
-			}(x, y)
-		}
+			}
+		}(x)
+
 	}
 	wg.Wait()
 }
